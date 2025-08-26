146e0070fa12a5294184db532b0fed5ab1b3956c
baquing <162646644+baquing@users.noreply.github.com>
2025-08-26 19:57:53 +0800

    Update php_practice.php

--- php_practice.php
+++ php_practice.php
@@ -65,7 +65,7 @@
         // echo"Bossing <br>";
         // print "Hello Boss!"
 
-        // data types
+    // data types
 
         // $a = ("malupiton");//string
         // $b = 1; // int
@@ -98,18 +98,279 @@
         // $a = "green";
         // $name = "<h1 style= 'color:$a'> Joel Malupiton";
 
-        $name = "joel malupiton";
-        echo strlen($name), "<br>";
-        echo str_word_count($name), "<br>";
-        echo strpos($name, "malupiton"), "<br>";
-        echo strtoupper($name), "<br>";
-        echo strtolower($name), "<br>";
-        echo str_replace("malupiton", "bossing", $name), "<br>";
-        echo strrev($name), "<br>";
-        echo trim($name), "<br>";
-        echo print_r( explode("l", $name), "<br>");
-        
+        // $name = "joel malupiton";
+        // echo strlen($name), "<br>";
+        // echo str_word_count($name), "<br>";
+        // echo strpos($name, "malupiton"), "<br>";
+        // echo strtoupper($name), "<br>";
+        // echo strtolower($name), "<br>";
+        // echo str_replace("malupiton", "bossing", $name), "<br>";
+        // echo strrev($name), "<br>";
+        // echo trim($name), "<br>";
+        // echo print_r( explode("l", $name), "<br>");
 
+
+    //Concatinate
+
+        // $x = "Hello";
+        // $y = "World!";
+        // echo $x ."  ". $y."<br>";
+        // echo "$x $y <br>";
+        // echo '$x $y <br>';  
+
+    //Slicing
+        // $x = "Hello";
+        // echo substr($x, 1, 4), "<br>";
+        // echo substr($x, 1), "<br>";
+        // echo substr($x, -1, 2), "<br>";
+
+        // Escape Characters
+        // $a = "sample";
+        // $x = "John"\"joel"\"Malupiton";
+        // echo $x;
+
+     //Numbers
+        // $a = -1;//int
+        // $b = 1.0;//float
+        // $c = "5";//String
+
+        // echo PHP_INT_MIN, "<br>";
+        // echo PHP_INT_MAX, "<br>";
+        // echo PHP_INT_SIZE, "<br>";
+        // echo is_int($c);
+
+        // echo PHP_FLOAT_MIN, ",<br>";
+        // echo PHP_FLOAT_MAX, ",<br>";
+        // echo PHP_FLOAT_DIG, ",<br>";
+        // echo PHP_FLOAT_EPSILON, ",<br>";
+        // ECHO is_float($b), "<br>";
+        // echo is_numeric($c), "<br>";
+ 
+    //PHP Casting - String
+
+        // $a = ("malupiton");//string
+        // $b = 1; // int
+        // $c = 3.14; // float
+        // $d = True; // bool
+        // $e = [1, 2, 3]; // array
+        // $f = new stdClass(); //object
+        // $g = null; //null
+        // $h = fopen("php_practice.php", "r");
+
+        //  $a = (string)$a;
+        //  $b = (string)$b;
+        //  $d = (string)$c;
+        //  $e = (string)$d;
+        //  $e = $e;
+        //  $f = $f;
+        //  $g = (string)$g;
+        //  $h = (string)$h;
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+     //Casting - integer
+
+        // $a = ("malupiton");//string
+        // $b = 1; // int
+        // $c = 3.14; // float
+        // $d = True; // bool
+        // $e = [1, 2, 3]; // array
+        // $f = new stdClass(); //object
+        // $g = null; //null
+        // $h = fopen("php_practice.php", "r");
+
+        // $a = (int)$a;
+        // $b = (int)$b;
+        // $c = (int)$c;
+        // $d = (int)$d;
+        // $e = (int)$e;
+        // $f = (int)$f;
+        // $g = (int)$g;
+        // $h = (int)$h;
+
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+        //Casting - Float
+
+        // $a = ("malupiton");//string
+        // $b = 1; // int
+        // $c = 3.14; // float
+        // $d = True; // bool
+        // $e = [1, 2, 3]; // array
+        // $f = new stdClass(); //object
+        // $g = null; //null
+        // $h = fopen("php_practice.php", "r");
+
+        // $a = (float)$a;
+        // $b = (float)$b;
+        // $c = (float)$c;
+        // $d = (float)$d;
+        // $e = (float)$e;
+        // $f = (float)$f;
+        // $g = (float)$g;
+        // $h = (float)$h;
+
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+    // Casting - Boolean
+        // $a = ("malupiton");//string
+        // $b = 1; // int
+        // $c = 3.14; // float
+        // $d = True; // bool
+        // $e = [1, 2, 3]; // array
+        // $f = new stdClass(); //object
+        // $g = null; //null
+        // $h = fopen("php_practice.php", "r");
+
+        // $a = (bool)$a;
+        // $b = (bool)$b;
+        // $c = (bool)$c;
+        // $d = (bool)$d;
+        // $e = (bool)$e;
+        // $f = (bool)$f;
+        // $g = (bool)$g;
+        // $h = (bool)$h;
+
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+    // Casting - Array
+
+        // $a = ("malupiton");//string
+        // $b = 1; // int
+        // $c = 3.14; // float
+        // $d = True; // bool
+        // $e = [1, 2, 3]; // array
+        // $f = new stdClass(); //object
+        // $g = null; //null
+        // $h = fopen("php_practice.php", "r");
+
+        // $a = (array)$a;
+        // $b = (array)$b;
+        // $c = (array)$c;
+        // $d = (array)$d;
+        // $e = (array)$e;
+        // $f = (array)$f;
+        // $g = (array)$g;
+        // $h = (array)$h;
+
+
+        // var_dump($a);
+        // echo "<br>";
+        // var_dump($b);
+        // echo "<br>";
+        // var_dump($c);
+        // echo "<br>";
+        // var_dump($d);
+        // echo "<br>";
+        // var_dump($e);
+        // echo "<br>";
+        // var_dump($f);
+        // echo "<br>";
+        // var_dump($g);
+        // echo "<br>";
+        // var_dump($h);
+        // echo "<br>";
+
+    // Casting - Null
+
+        $a = ("malupiton");//string
+        $b = 1; // int
+        $c = 3.14; // float
+        $d = True; // bool
+        $e = [1, 2, 3]; // array
+        $f = new stdClass(); //object
+        $g = null; //null
+        $h = fopen("php_practice.php", "r");
+
+        $a = (unset)$a;
+        $b = (unset)$b;
+        $c = (unset)$c;
+        $d = (unset)$d;
+        $e = (unset)$e;
+        $f = (unset)$f;
+        $g = (unset)$g;
+        $h = (unset)$h;
+
+
+        var_dump($a);
+        echo "<br>";
+        var_dump($b);
+        echo "<br>";
+        var_dump($c);
+        echo "<br>";
+        var_dump($d);
+        echo "<br>";
+        var_dump($e);
+        echo "<br>";
+        var_dump($f);
+        echo "<br>";
+        var_dump($g);
+        echo "<br>";
+        var_dump($h);
+        echo "<br>";
     ?>
 </body>
 </html>
